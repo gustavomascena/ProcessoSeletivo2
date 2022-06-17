6a1260261a666a2091375a8c486d7a8496892813
Metrodata <Metrodata@METRODATA_ELE>
2022-06-17 18:02:59 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o texto e tecle enter");
+            string texto = (Console.ReadLine());
+            int quantidadeCaracteres = texto.Length;
+            for (int i = quantidadeCaracteres; i != 0; i--)
+            {
+                Console.Write(texto.ElementAt(i - 1));
+            }
+        }
+    }
+}
